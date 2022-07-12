c4e1c76002821a624acbf8b7c2008fcc89cd4e9e
Ericchuks <you@example.com>
2022-07-13 00:52:21 +0100

    updated

---
+++ script.js
@@ -0,0 +1,19 @@
+let close = document.querySelector(".close");
+let rulesBtn = document.querySelector(".rules");
+let modal = document.querySelector(".modal");
+let gameBoard = document.querySelector(".step1");
+
+function closeRule(){
+    if(modal.style.display === "none"){
+        modal.style.display = "block";
+        gameBoard.style.display = "none"
+        console.log("@@@")
+    }else{
+        modal.style.display = "none";
+        gameBoard.style.display = "block"
+        console.log("@")
+    }
+}
+
+close.addEventListener("click", closeRule);
+rulesBtn.addEventListener("click", closeRule);
